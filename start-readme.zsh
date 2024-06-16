0d21a4dd84e4143a92571831d8c88b14c5581b0e
Emre Sahin <github@emresult.com>
2024-06-16 13:03:48 +0300

    Completed Readme Notebook

--- start-readme.zsh
+++ start-readme.zsh
@@ -8,8 +8,8 @@ source .venv/bin/activate
 
 maturin develop -m ../xvc.py/Cargo.toml
 
-for d in test-data .xvc .git ; do
-  if [ -d Readme/$d ] ; then
+for d in test-data .xvc .git .gitignore ; do
+  if [ -e Readme/$d ] ; then
     chmod -R +w Readme/${d}
     rm -rf Readme/${d}
   fi
@@ -17,8 +17,9 @@ done
 
 PORT=7979
 if [[ -z "$(ps ax | rg jupyter-lab | rg -v ps | rg $PORT)" ]] ; then
-  jupyter lab --port=$PORT --notebook-dir=Readme/ &
-  open http://localhost:${PORT}/lab/workspaces/auto-H/tree/Readme.ipynb
+  kill $(ps ax | rg jupyter-lab | rg -v ps | rg $PORT | cut -f 1 -d ' ')
 fi
+
+jupyter lab --port=$PORT --notebook-dir=Readme/ && open http://localhost:${PORT}/lab/workspaces/auto-H/tree/Readme.ipynb
 
 
